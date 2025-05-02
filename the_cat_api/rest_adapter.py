738566807d5f13a4085f8a040be08eed9a86eb7a
bellygoblin <brian@mitchellfamily.me>
2025-05-02 17:11:40 -0400

    Clean up code order

--- the_cat_api/rest_adapter.py
+++ the_cat_api/rest_adapter.py
@@ -1,28 +1,21 @@
 import requests
 import requests.packages
+import logging
 from typing import List, Dict
 from json import JSONDecodeError
 from the_cat_api.exceptions import TheCatApiException
 from the_cat_api.models import Result
 
 class RestAdapter:
-  def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True):
+  def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True, logger: logging.Logger = None):
     self.url = "https://{}/{}/".format(hostname, ver)
     self._api_key = api_key
     self._ssl_verify = ssl_verify
+    self._logger = logger or logging.getLogger(__name__)
     if not ssl_verify:
       # noinspection PyUnresolvedReferences
       requests.packages.urllib3.disable_warings()
 
-  def get(self, endpoint: str, ep_params: Dict = None) -> Result:
-    return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
-  
-  def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
-    return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
-  
-  def delete(self, endpint: str, ep_params: Dict = None, data: Dict = None) -> Result:
-    return self._do(http_method='DELETE', endpoint=endpint, ep_params=ep_params, data=data)
-  
   def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
     full_url = self.url + endpoint
     headers = {'x-api-key': self._api_key}
@@ -40,3 +33,13 @@ class RestAdapter:
     if 299 >= response.status_code >= 200:     # OK
       return Result(response.status_code, message=response.reason, data=data_out)
     raise TheCatApiException(f"{response.status_code}: {response.reason}")
+  
+  def get(self, endpoint: str, ep_params: Dict = None) -> Result:
+    return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
+  
+  def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
+  
+  def delete(self, endpint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    return self._do(http_method='DELETE', endpoint=endpint, ep_params=ep_params, data=data)
+  
